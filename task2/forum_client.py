f456373a9c16e50629bf9fb53b963f7de72a1e9d
Dmitry Lichmanov <dimlichmanov@gmail.com>
2020-10-14 21:36:27 +0300

    Pattern for 2 task

---
+++ task2/forum_client.py
@@ -0,0 +1,9 @@
+class ForumClient:
+    def __init__(self, logical_time, nickname):
+        self.logical_time = logical_time
+        self.nickname = nickname
+
+    def init_connection(self):
+        self.logical_time = 1
+
+
