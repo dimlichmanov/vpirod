f1989afcd83a369c946e3e6c92b98a0c33300a40
Dmitry Lichmanov <dimlichmanov@gmail.com>
2020-10-15 16:16:14 +0300

    rebased server interface

--- task2/forum_server.py
+++ task2/forum_server.py
@@ -7,12 +7,19 @@ import threading
 class ForumServer:
     def __init__(self, logical_time=0):
         self.logical_time = logical_time
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.channel = connection.channel()
-        self.channel.exchange_declare(exchange='send_from_server', exchange_type='fanout')
 
-        result = self.channel.queue_declare(queue='', exclusive=False)
+        # Initialize connections
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel_receive = connection.channel()
+        self.channel_send = connection.channel()
+
+        # Configure "send" channel
+        self.channel_send.exchange_declare(exchange='send_from_server', exchange_type='fanout')
+
+        # Configure "receive" channel - an only queue from clients
+        result = self.channel_receive.queue_declare(queue='', exclusive=True)
         self.queue_name = result.method.queue
+        self.queue_name.bind(exchange='send_to_server')
 
     def update_time(self, new_time):
         if self.logical_time > new_time:
@@ -21,48 +28,26 @@ class ForumServer:
             self.logical_time = new_time + 1
 
     def process_message(self, message):
+        # Split a message, retrieve a logical time, send a client-friendly message to clients
         elements = message.split('_')
-        new_logical_time = [-1]
+        new_logical_time = elements[-1]
         self.update_time(new_logical_time)
-        print('User {} replied to message {} : {} (log_time : {})'.format(*elements))
+        posting = 'User {} replied to message {} : {} (time : {})'.format(*elements)
+        message = posting + '_' + new_logical_time
+        self.send(message)
 
-    def send(self, text_message, response_to):
-        self.update_time(-1)  # check if consuming thread updated a logical time
-        message = '{}_{}_{}_{}'.format(self.nickname, text_message, response_to, self.logical_time)
-        self.channel.basic_publish(exchange='logs', routing_key='', body=message)
+    def send(self, message):
+        self.channel_send.basic_publish(exchange='send_from_server', routing_key='', body=message)
+        self.start_consumer()
 
     def callback_actions(self, ch, method, properties, body):
-        debug_mode = 1
-        with open('ny.osm') as osm_file:
-            streets_all = soup.find_all('way')
-            spisok = []
-            i = 0
-            for streets in streets_all:
-                i += 1
-                b = streets.find_all('tag')
-                flag = 0
-                for tag_line in b:
-                    a = tag_line['k']
-                    if 'highway' == a:
-                        flag = 1
-                    if 'name' == a and flag == 1 and tag_line['v'][0] == chr(letter):
-                        spisok.append(tag_line['v'])
-
-            spisok = pd.Series(spisok)
-            spisok.drop_duplicates(inplace=True)
-            if debug_mode == 0:
-                message = str(len(spisok))
-            else:
-                for street in spisok:
-                    print(street)
-                message = str(len(spisok))
+        self.process_message(body)
 
     def start_consumer(self):
-        self.channel.basic_consume(
+        self.channel_receive.basic_consume(
             queue=self.queue_name, on_message_callback=self.callback_actions(), auto_ack=True)
-        self.channel.start_consuming()
+        self.channel_send.start_consuming()
 
 
 if __name__ == '__main__':
-    client = ForumServer()
-    #client.send('Privet', 0)
+    server = ForumServer()
