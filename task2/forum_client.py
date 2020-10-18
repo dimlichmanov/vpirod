12fade0cbe07b7a51e7569e660ea9f574fbd5c16
Dmitry Lichmanov <dimlichmanov@gmail.com>
2020-10-18 03:36:03 +0300

    Ready version

--- task2/forum_client.py
+++ task2/forum_client.py
@@ -9,62 +9,66 @@ class ForumClient:
     def __init__(self, nickname, logical_time=0):
         self.logical_time = logical_time
         self.nickname = nickname
+        self.update_lock = threading.Lock()
 
-        # Initialize "send" connection on a master thread
-        self.connection_send = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.channel_send = self.connection_send.channel()
-
-        # Configure "send" channel
-        self.channel_send.exchange_declare(exchange='send_to_server', exchange_type='fanout')
-
-        # Initialize a listening thread
-        consumer_thread = threading.Thread(target=self.start_consumer)
-        consumer_thread.start()
-
-
-    def update_time(self, new_time):
-        # Update logical time through a critical section
-        #update_lock = threading.Lock()
-        #update_lock.acquire()
-        if self.logical_time > new_time:
-            self.logical_time += 1
-        else:
-            self.logical_time = new_time + 1
-        #update_lock.release()
-
-    def send(self, text_message, response_to):
-        # Update logical time and send a message
-        #self.update_time(-1)
-        message = '{}_{}_{}_{}'.format(self.nickname, text_message, response_to, self.logical_time)
-        self.channel_send.basic_publish(exchange='send_to_server', routing_key='', body=message)
-
-    def callback_actions(self, ch, method, properties, body):
-        # Update local logic time and sleep again
-        print(body)
-        received_time = int(body.split('_')[-1])
-        self.update_time(received_time)
-        # TODO check if start_consuming checks already present messages
-
-    def consume(self):
-        # Set consumer parameters and sleep on a reading queue
-        self.channel_receive.basic_consume(
-            queue=self.queue_name, on_message_callback=self.callback_actions, auto_ack=True)
-        time.sleep(15)
-        self.channel_receive.start_consuming()
-
-    def start_consumer(self):
-        # Pika is not safe with a shared Connection - initialize new connection in new thread
         self.connection_receive = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel_receive = self.connection_receive.channel()
-
-        # Configure "receive" channel - unique queue for each client
         result = self.channel_receive.queue_declare(queue=self.nickname, exclusive=True)
         self.queue_name = result.method.queue
         self.channel_receive.exchange_declare(exchange='send_from_server', exchange_type='fanout')
         self.channel_receive.queue_bind(exchange='send_from_server', queue=self.queue_name)
-        self.consume()
+
+        consumer_thread = threading.Thread(target=self.start_consumer)
+        consumer_thread.start()
+
+        # Initialize "send" connection on a master thread
+        self.connection_send = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel_send = self.connection_send.channel()
+        self.channel_send.exchange_declare(exchange='send_to_server', exchange_type='fanout')
+
+    def update_time(self, new_time):
+        # Update logical time through a critical section
+
+        self.update_lock.acquire()
+        if self.logical_time > new_time:
+            self.logical_time += 1
+        else:
+            self.logical_time = new_time + 1
+        self.update_lock.release()
+
+    def send(self, text_message, response_to):
+        # Update logical time and send a message
+        #self.update_time(-1)
+        message = '{}_{}_{}_{}'.format(self.nickname, response_to, text_message, self.logical_time)
+        self.channel_send.basic_publish(exchange='send_to_server', routing_key='', body=message)
+
+    def callback_actions(self, ch, method, properties, body):
+        # Update local logic time and sleep again
+        message = body.decode("utf-8").split('_')
+        print(message[:-1][0])
+        received_time = int(message[-1])
+        self.update_time(received_time)
+
+    def start_consumer(self):
+        # Set consumer parameters and sleep on a reading queue
+        self.channel_receive.basic_consume(
+            queue=self.queue_name, on_message_callback=self.callback_actions, auto_ack=True)
+        self.channel_receive.start_consuming()
 
 
 if __name__ == '__main__':
-    client = ForumClient(nickname='Dmitry', logical_time=0)
-    client.send('Privet', 0)
+    initial = str(input("Enter yor nickname and logical time: "))
+    nickname = str(initial.split('_')[0])
+    log_time = int(initial.split('_')[1])
+
+    client = ForumClient(nickname=nickname, logical_time=log_time)
+
+    message = str(input("Enter yor message: "))
+    text = str(message.split('_')[0])
+    response = int(message.split('_')[1])
+    while message:
+        client.send(message, response)
+        time.sleep(3)  # ONLY FOR STDIN
+        message = str(input("Enter yor message: "))
+        text = str(message.split('_')[0])
+        response = int(message.split('_')[1])
--- task2/forum_server.py
+++ task2/forum_server.py
@@ -1,26 +1,27 @@
-import sys
 import pika
-import time
-import threading
 
 
 class ForumServer:
     def __init__(self, logical_time=0):
         self.logical_time = logical_time
+        self.message_counter = 1
 
         # Initialize connections
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.channel_receive = connection.channel()
-        self.channel_send = connection.channel()
+        self.connection_send = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.connection_receive = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel_receive = self.connection_receive.channel()
+        self.channel_send = self.connection_send.channel()
 
         # Configure "send" channel
         self.channel_send.exchange_declare(exchange='send_from_server', exchange_type='fanout')
+        self.channel_receive.exchange_declare(exchange='send_to_server', exchange_type='fanout')
 
         # Configure "receive" channel - an only queue from clients
         result = self.channel_receive.queue_declare(queue='server', exclusive=True)
         self.queue_name = result.method.queue
-        self.channel_receive.exchange_declare(exchange='send_to_server', exchange_type='fanout')
         self.channel_receive.queue_bind(exchange='send_to_server', queue=self.queue_name)
+
+        print('#0 - Welcome to chat')
 
         # Sleeping on receive channel
         self.start_consumer()
@@ -31,26 +32,17 @@ class ForumServer:
         else:
             self.logical_time = new_time + 1
 
-    def process_message(self, message):
-        # Split a message, retrieve a logical time, send a client-friendly message to clients
-        elements = str(message).split('_')
-        #new_logical_time = elements[-1]
-        #self.update_time(new_logical_time)
-        posting = 'User {} replied to message {} : {} (time : {})'.format(*elements)
-        print(posting)
-        #message = posting + '_' + new_logical_time
-        message = posting + '_' + '0'
-        self.send(message)
-
-    def send(self, message):
-        self.channel_send.basic_publish(exchange='send_from_server', routing_key='', body=message)
-        print('kek')
-        #self.start_consumer()
-
     def callback_actions(self, ch, method, properties, body):
-        self.process_message(body)
-        print(body)
-        #self.send(body)
+        # Split a message, retrieve a logical time, send a client-friendly message to clients
+        elements = body.decode("utf-8").split('_')
+        new_logical_time = int(elements[-1])
+        self.update_time(new_logical_time)
+        print(self.logical_time)
+        posting = '#{} - User {} replied to message {} : {} (time : {})'.format(self.message_counter, *elements[:-1], self.logical_time)
+        print(posting)
+        self.message_counter += 1
+        message = posting + '_' + str(new_logical_time)
+        self.channel_send.basic_publish(exchange='send_from_server', routing_key='', body=message)
 
     def start_consumer(self):
         self.channel_receive.basic_consume(
