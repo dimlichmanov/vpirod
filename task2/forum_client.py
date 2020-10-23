1f708fe14584beb56b4a32b6bc5c42f4ff8392e4
Dmitry Lichmanov <dimlichmanov@gmail.com>
2020-10-23 17:26:32 +0300

    Complete version

--- task2/forum_client.py
+++ task2/forum_client.py
@@ -9,6 +9,7 @@ class ForumClient:
         self.logical_time = logical_time
         self.nickname = nickname
         self.update_lock = threading.Lock()
+        self.response = None
 
         self.connection_receive = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel_receive = self.connection_receive.channel()
@@ -16,11 +17,11 @@ class ForumClient:
         self.queue_name = result.method.queue
         self.channel_receive.exchange_declare(exchange='send_from_server', exchange_type='fanout')
         self.channel_receive.queue_bind(exchange='send_from_server', queue=self.queue_name)
+        self.channel_receive.basic_consume(
+            queue=self.queue_name,
+            on_message_callback=self.callback_actions,
+            auto_ack=True)
 
-        consumer_thread = threading.Thread(target=self.start_consumer)
-        consumer_thread.start()
-
-        # Initialize "send" connection on a master thread
         self.connection_send = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel_send = self.connection_send.channel()
         self.channel_send.exchange_declare(exchange='send_to_server', exchange_type='fanout')
@@ -37,21 +38,29 @@ class ForumClient:
 
     def send(self, text_message, response_to):
         # Update logical time and send a message
-        message_send = '{}_{}_{}_{}'.format(self.nickname, response_to, text_message, self.logical_time)
+        message_send = '{}_{}_{}_{}'.format(self.nickname, text_message, response_to, self.logical_time)
+        self.channel_send.basic_publish(exchange='send_to_server', routing_key='', properties=pika.BasicProperties(reply_to=self.queue_name), body=message_send)
+
+    def get_chat(self):
+        message_send = '{}_{}'.format(self.nickname, self.logical_time)
         self.channel_send.basic_publish(exchange='send_to_server', routing_key='', body=message_send)
+        resp = self.start_consumer()
+        print(resp)
+        self.response = None
 
     def callback_actions(self, ch, method, properties, body):
         # Update local logic time and sleep again
         message_rec = body.decode("utf-8").split('_')
-        print(message_rec[:-1][0])
-        received_time = int(message_rec[-1])
-        self.update_time(received_time)
+        print(message_rec)
+        # received_time = int(message_rec[-1])
+        # self.update_time(received_time)
+        self.response = str(message_rec[0])
 
     def start_consumer(self):
         # Set consumer parameters and sleep on a reading queue
-        self.channel_receive.basic_consume(
-            queue=self.queue_name, on_message_callback=self.callback_actions, auto_ack=True)
-        self.channel_receive.start_consuming()
+        while self.response is None:
+            self.connection_receive.process_data_events()
+        return self.response
 
 
 if __name__ == '__main__':
@@ -62,11 +71,18 @@ if __name__ == '__main__':
     client = ForumClient(nickname=nick, logical_time=log_time)
 
     message = str(input("Enter yor message: "))
-    text = str(message.split('_')[0])
-    response = int(message.split('_')[1])
-    while message:
-        client.send(message, response)
-        time.sleep(3)  # ONLY FOR STDIN
-        message = str(input("Enter yor message: "))
+    parts = message.split('_')
+    if len(parts) == 2:
         text = str(message.split('_')[0])
         response = int(message.split('_')[1])
+    while message:
+        if len(parts) == 2:
+            client.send(message, response)
+        else:
+            client.get_chat()
+        time.sleep(3)  # ONLY FOR STDIN
+        message = str(input("Enter yor message: "))
+        parts = message.split('_')
+        if len(parts) == 2:
+            text = str(message.split('_')[0])
+            response = int(message.split('_')[1])
--- task2/forum_server.py
+++ task2/forum_server.py
@@ -5,6 +5,10 @@ class ForumServer:
     def __init__(self, logical_time=0):
         self.logical_time = logical_time
         self.message_counter = 1
+        init_message = 'Welcome to chat (time={})'.format(self.logical_time)
+        self.messages = [init_message]
+        self.hierarchy = [[]]
+        self.current_history=""
 
         # Initialize connections
         self.connection_send = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -21,8 +25,6 @@ class ForumServer:
         self.queue_name = result.method.queue
         self.channel_receive.queue_bind(exchange='send_to_server', queue=self.queue_name)
 
-        print('#0 - Welcome to chat')
-
         # Sleeping on receive channel
         self.start_consumer()
 
@@ -32,16 +34,29 @@ class ForumServer:
         else:
             self.logical_time = new_time + 1
 
+    def gain_history(self, num_thread, num_tabs):
+        for i in self.hierarchy[num_thread]:
+            self.current_history += (str('\t')*num_tabs + str(self.messages[i]) + str('\n'))
+            if len(self.hierarchy[i]) > 0:
+                self.gain_history(i, num_tabs+1)
+
     def callback_actions(self, ch, method, properties, body):
         # Split a message, retrieve a logical time, send a client-friendly message to clients
         elements = body.decode("utf-8").split('_')
         new_logical_time = int(elements[-1])
         self.update_time(new_logical_time)
-        posting = '#{} - User {} replied to message {} : {} (time : {})'.format(self.message_counter, elements[0],elements[1],elements[2], self.logical_time)
-        print(posting)
-        self.message_counter += 1
-        message = posting + '_' + str(self.logical_time)
-        self.channel_send.basic_publish(exchange='send_from_server', routing_key='', body=message)
+        if len(elements) > 2:
+            response_to = int((elements[-2]))
+            self.hierarchy.append([])
+            self.hierarchy[response_to].append(self.message_counter)
+            self.messages.append('#{} - {} answers on {}: '.format(self.message_counter, elements[0], response_to) + str(elements[-4]) + '(time:{})'.format(self.logical_time))
+            self.message_counter += 1
+            print(self.hierarchy)
+        else:
+            self.current_history = self.messages[0]+'\n'
+            self.gain_history(0, 1)
+            print(self.current_history)
+            self.channel_send.basic_publish(exchange='', routing_key=str(elements[0]), body=self.current_history)
 
     def start_consumer(self):
         self.channel_receive.basic_consume(
